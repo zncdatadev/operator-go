d03da2131a9e71bae4ce6294537a7b39d4a92b6e
wastill <2225144254@qq.com>
2023-12-25 18:23:47 +0800

    fix: repaire ci 1. add golangci timeout 2. complete database provider 3. complete condition

--- pkg/status/conditions.go
+++ pkg/status/conditions.go
@@ -2,18 +2,23 @@ package status
 
 // ConditionType is the type of condition
 const (
-	ConditionTypeProgressing string = "Progressing"
-	ConditionTypeReconcile   string = "Reconcile"
-	ConditionTypeAvailable   string = "Available"
+	ConditionTypeProgressing         string = "Progressing"
+	ConditionTypeReconcile           string = "Reconcile"
+	ConditionTypeAvailable           string = "Available"
+	ConditionTypeReconcilePVC        string = "ReconcilePVC"
+	ConditionTypeReconcileService    string = "ReconcileService"
+	ConditionTypeReconcileIngress    string = "ReconcileIngress"
+	ConditionTypeReconcileDeployment string = "ReconcileDeployment"
+	ConditionTypeReconcileSecret     string = "ReconcileSecret"
+	ConditionTypeReconcileDaemonSet  string = "ReconcileDaemonSet"
+	ConditionTypeReconcileConfigMap  string = "ReconcileConfigMap"
 )
 
 // ConditionReason is the reason for the condition
 const (
-	ConditionReasonPreparing           string = "Preparing"
-	ConditionReasonRunning             string = "Running"
-	ConditionReasonConfig              string = "Config"
-	ConditionReasonReconcilePVC        string = "ReconcilePVC"
-	ConditionReasonReconcileService    string = "ReconcileService"
-	ConditionReasonReconcileIngress    string = "ReconcileIngress"
-	ConditionReasonReconcileDeployment string = "ReconcileDeployment"
+	ConditionReasonPreparing string = "Preparing"
+	ConditionReasonRunning   string = "Running"
+	ConditionReasonConfig    string = "Config"
+	ConditionReasonReady     string = "Ready"
+	ConditionReasonFail      string = "Fail"
 )
