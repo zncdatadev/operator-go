0e7fe8de33150a7fbd4c3db1f577406655eac514
whg517 <huagang517@126.com>
2024-05-24 16:26:15 +0800

    feat: add common api (#42)
    - cluster operation
    - logging
    - pdb
    - resource

---
+++ pkg/apis/commons/v1alpha1/cluster_operation.go
@@ -0,0 +1,12 @@
+package v1alpha1
+
+// ClusterOperationSpec defines the desired state of ClusterOperation
+type ClusterOperationSpec struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=false
+	ReconciliationPaused bool `json:"reconciliationPaused,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=false
+	Stopped bool `json:"stopped,omitempty"`
+}
---
+++ pkg/apis/commons/v1alpha1/logging_types.go
@@ -0,0 +1,32 @@
+package v1alpha1
+
+type LoggingConfigSpec struct {
+	// +kubebuilder:validation:Optional
+	Loggers map[string]*LogLevelSpec `json:"loggers,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Console *LogLevelSpec `json:"console,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	File *LogLevelSpec `json:"file,omitempty"`
+}
+
+// LogLevelSpec
+// level mapping example:
+//
+// |---------------------|-----------------|
+// |  App log level      |  kds log level  |
+// |---------------------|-----------------|
+// |  CRITICAL           |  FATAL          |
+// |  ERROR              |  ERROR          |
+// |  WARNING            |  WARN           |
+// |  INFO               |  INFO           |
+// |  DEBUG              |  DEBUG          |
+// |  DEBUG              |  TRACE          |
+// |---------------------|-----------------|
+type LogLevelSpec struct {
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:="INFO"
+	// +kubebuilder:validation:Enum=FATAL;ERROR;WARN;INFO;DEBUG;TRACE
+	Level string `json:"level,omitempty"`
+}
