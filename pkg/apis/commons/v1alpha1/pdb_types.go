9fac43126bfbd31f8e6c9ea2dff987060d8a695a
whg517 <huagang517@126.com>
2024-05-24 16:17:34 +0800

    feat: add common api
    - cluster operation
    - logging
    - pdb
    - resource

---
+++ pkg/apis/commons/v1alpha1/pdb_types.go
@@ -0,0 +1,9 @@
+package v1alpha1
+
+type PodDisruptionBudgetSpec struct {
+	// +kubebuilder:validation:Optional
+	MinAvailable int32 `json:"minAvailable,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	MaxUnavailable int32 `json:"maxUnavailable,omitempty"`
+}
