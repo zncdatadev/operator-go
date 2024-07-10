83771ce64de2bea24b499256649dfa86ab9e4a41
whg517 <huagang517@126.com>
2024-07-10 20:58:50 +0800

    feat: update XMLConfiguration and add test

--- pkg/util/xml.go
+++ pkg/util/xml.go
@@ -1,117 +1,110 @@
 package util
 
 import (
-	"bytes"
 	"encoding/xml"
+	"slices"
 )
 
-type XmlNameValuePair struct {
-	Name  string `xml:"name"`
-	Value string `xml:"value"`
+const (
+	XMLStylesheet = `<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>` + "\n"
+)
+
+type configuration struct {
+	XMLName    xml.Name   `xml:"configuration"`
+	Properties []Property `xml:"property"`
 }
 
-type XmlConfiguration struct {
-	XMLName    xml.Name           `xml:"configuration"`
-	Properties []XmlNameValuePair `xml:"property"`
+type Property struct {
+	XMLName     xml.Name `xml:"property"`
+	Comment     string   `xml:",comment"`
+	Name        string   `xml:"name"`
+	Value       string   `xml:"value"`
+	Description string   `xml:"description,omitempty"`
 }
 
-func NewXmlConfiguration(properties []XmlNameValuePair) *XmlConfiguration {
-	return &XmlConfiguration{
-		Properties: properties,
+type XMLConfiguration struct {
+	Properties    []Property
+	XMLStylesheet string
+}
+
+func NewXMLConfiguration() *XMLConfiguration {
+	return &XMLConfiguration{
+		Properties:    []Property{},
+		XMLStylesheet: XMLStylesheet,
 	}
 }
 
-func (c *XmlConfiguration) String(properties []XmlNameValuePair) string {
-	if len(c.Properties) != 0 {
-		c.Properties = c.DistinctProperties(properties)
+func NewXMLConfigurationFromString(xmlString string) (*XMLConfiguration, error) {
+	x := configuration{}
+	err := xml.Unmarshal([]byte(xmlString), &x)
+	if err != nil {
+		return nil, err
 	}
-	buf := new(bytes.Buffer)
-	if _, err := buf.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"); err != nil {
-		logger.Error(err, "failed to write xml document head")
-	}
-	enc := xml.NewEncoder(buf)
-	enc.Indent("", "  ")
-	if err := enc.Encode(c); err != nil {
-		logger.Error(err, "failed to encode xml document")
-		panic(err)
-	}
-	return buf.String()
+	return &XMLConfiguration{Properties: x.Properties, XMLStylesheet: XMLStylesheet}, nil
 }
 
-// DistinctProperties distinct properties by name,
-func (c *XmlConfiguration) DistinctProperties(properties []XmlNameValuePair) []XmlNameValuePair {
-	var collect []XmlNameValuePair
-	collect = append(collect, c.Properties...)
-	collect = append(collect, properties...)
+func NewXMLConfigurationFromMap(properties map[string]string) *XMLConfiguration {
+	x := NewXMLConfiguration()
+	x.AddPropertiesWithMap(properties)
+	return x
+}
 
-	var distinctProperties []XmlNameValuePair
-	var distinctKeys map[string]int
-	for idx, v := range collect {
-		if distinctKeys == nil {
-			distinctKeys = make(map[string]int)
-		}
-		if existIdx, ok := distinctKeys[v.Name]; !ok {
-			distinctKeys[v.Name] = idx
-			distinctProperties = append(distinctProperties, v)
-		} else {
-			distinctProperties[existIdx] = v
+func (x *XMLConfiguration) GetProperty(name string) (Property, bool) {
+	for _, p := range x.Properties {
+		if p.Name == name {
+			return p, true
 		}
 	}
-	return distinctProperties
-
-	//var distinctMap = make(map[string]XmlNameValuePair)
-	//for _, v := range collect {
-	//	distinctMap[v.Name] = v
-	//}
-	//return maps.Values(distinctMap)
+	return Property{}, false
 }
 
-func (c *XmlConfiguration) StringWithProperties(properties map[string]string) string {
-	var pairs []XmlNameValuePair
-	for k, v := range properties {
-		pairs = append(pairs, XmlNameValuePair{
-			Name:  k,
-			Value: v,
-		})
-	}
-	return c.String(pairs)
-}
-
-// Append  to exist xml dom
-func Append(originXml string, properties []XmlNameValuePair) string {
-	var xmlDom XmlConfiguration
-	//string -> dom
-	if err := xml.Unmarshal([]byte(originXml), &xmlDom); err != nil {
-		panic(err)
-	}
-	return xmlDom.String(properties)
-}
-
-// OverrideXmlContent overrides the content of a xml file
-// append the override properties to the current xml dom
-func OverrideXmlContent(current string, overrideProperties map[string]string) string {
-	var xmlDom XmlConfiguration
-	//string -> dom
-	if err := xml.Unmarshal([]byte(current), &xmlDom); err != nil {
-		panic(err)
-	}
-	// do override
-	for k, v := range overrideProperties {
-		overridePair := XmlNameValuePair{
-			Name:  k,
-			Value: v,
+func (x *XMLConfiguration) AddProperty(p Property) {
+	for i, existingProperty := range x.Properties {
+		if existingProperty.Name == p.Name {
+			x.Properties[i] = p // update
+			return
 		}
-		xmlDom.Properties = append(xmlDom.Properties, overridePair)
 	}
-	// dom -> string
-	var b bytes.Buffer
-	if _, err := b.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"); err != nil {
-		logger.Error(err, "failed to write string")
+	x.Properties = append(x.Properties, p) // add
+}
+
+func (x *XMLConfiguration) AddPropertyWithString(name, value, description, comment string) {
+	x.AddProperty(Property{Name: name, Value: value, Description: description, Comment: comment})
+}
+
+func (x *XMLConfiguration) AddPropertiesWithMap(properties map[string]string) {
+	for name, value := range properties {
+		x.AddProperty(Property{Name: name, Value: value})
 	}
-	encoder := xml.NewEncoder(&b)
-	encoder.Indent("", "  ")
-	if err := encoder.Encode(xmlDom); err != nil {
-		logger.Error(err, "failed to encode xml")
+}
+
+func (x *XMLConfiguration) DeleteProperties(names ...string) {
+	s := slices.DeleteFunc(x.Properties, func(i Property) bool {
+		for _, name := range names {
+			if i.Name == name {
+				return true
+			}
+		}
+		return false
+	})
+	x.Properties = s
+}
+
+func (x *XMLConfiguration) getHeader() string {
+	if x.XMLStylesheet == "" {
+		return xml.Header + XMLStylesheet
 	}
-	return b.String()
+	return xml.Header + x.XMLStylesheet
+}
+
+func (x *XMLConfiguration) Marshal() (string, error) {
+	c := &configuration{Properties: x.Properties}
+	data, err := xml.MarshalIndent(c, "", "    ")
+	if err != nil {
+		return "", err
+	}
+
+	fullXML := x.getHeader() + string(data)
+
+	return fullXML, nil
 }
--- pkg/util/xml_test.go
+++ pkg/util/xml_test.go
@@ -1,71 +1,232 @@
 package util
 
-import (
-	"testing"
-)
+import "testing"
 
-func TestXmlConfiguration_Append(t *testing.T) {
+func TestMarshal(t *testing.T) {
 
-	const origin = `<?xml version="1.0" encoding="UTF-8"?>
-	<configuration>
-		<property>
-			<name>key1</name>
-			<value>value1</value>
-		</property>
-	</configuration>`
-
-	type args struct {
-		originXml  string
-		properties []XmlNameValuePair
-	}
 	tests := []struct {
 		name string
-		args args
+		x    *XMLConfiguration
 		want string
 	}{
-		// TODO: Add test cases.
 		{
-			name: "append a new property",
-			args: args{
-				originXml:  origin,
-				properties: []XmlNameValuePair{{Name: "key2", Value: "value2"}},
+			name: "empty",
+			x:    &XMLConfiguration{},
+			want: `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+<configuration></configuration>`,
+		},
+		{
+			name: "one",
+			x: &XMLConfiguration{
+				Properties: []Property{
+					{
+						Name:        "name",
+						Value:       "value",
+						Description: "description",
+						Comment:     "This is a comment",
+					},
+				},
 			},
 			want: `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
-  <property>
-    <name>key1</name>
-    <value>value1</value>
-  </property>
-  <property>
-    <name>key2</name>
-    <value>value2</value>
-  </property>
+    <property>
+        <!--This is a comment-->
+        <name>name</name>
+        <value>value</value>
+        <description>description</description>
+    </property>
 </configuration>`,
 		},
 		{
-			name: "append a new property, but the name exists in origin xml, should override it",
-			args: args{
-				originXml:  origin,
-				properties: []XmlNameValuePair{{Name: "key1", Value: "value2"}, {Name: "key3", Value: "value3"}},
+			name: "two",
+			x: &XMLConfiguration{
+				Properties: []Property{
+					{
+						Name:  "name1",
+						Value: "value1",
+					},
+				},
 			},
 			want: `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
-  <property>
-    <name>key1</name>
-    <value>value2</value>
-  </property>
-  <property>
-    <name>key3</name>
-    <value>value3</value>
-  </property>
+    <property>
+        <name>name1</name>
+        <value>value1</value>
+    </property>
 </configuration>`,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Append(tt.args.originXml, tt.args.properties); got != tt.want {
-				t.Errorf("XmlConfiguration.Append() = %v, want %v", got, tt.want)
+			got, err := tt.x.Marshal()
+			if err != nil {
+				t.Errorf("Marshal() error = %v", err)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Marshal() got = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+// func TestNewXMLConfigurationFromString(t *testing.T) {
+// 	xmlString := `<?xml version="1.0" encoding="UTF-8"?>
+// <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+// <configuration>
+//     <property>
+//         <name>name</name>
+//         <value>value</value>
+//     </property>
+// </configuration>`
+
+// 	x, err := NewXMLConfigurationFromString(xmlString)
+// 	if err != nil {
+// 		t.Errorf("NewXMLConfigurationFromString() error = %v", err)
+// 		return
+// 	}
+
+// 	if len(x.Properties) != 1 {
+// 		t.Errorf("NewXMLConfigurationFromString() failed, expected 1 property, got %d", len(x.Properties))
+// 	} else {
+// 		p := x.Properties[0]
+// 		if p.Name != "name" || p.Value != "value" || p.Description != "description" || p.Comment != "This is a comment" {
+// 			t.Errorf("NewXMLConfigurationFromString() failed, expected property %v, got %v", Property{Name: "name", Value: "value", Description: "description", Comment: "This is a comment"}, p)
+// 		}
+// 	}
+// }
+
+func TestNewXMLConfigurationFromMap(t *testing.T) {
+	properties := map[string]string{
+		"name1": "value1",
+		"name2": "value2",
+		"name3": "value3",
+	}
+
+	x := NewXMLConfigurationFromMap(properties)
+
+	if len(x.Properties) != len(properties) {
+		t.Errorf("NewXMLConfigurationFromMap() failed, expected %d properties, got %d", len(properties), len(x.Properties))
+	} else {
+		for name, value := range properties {
+			p, found := x.GetProperty(name)
+			if !found {
+				t.Errorf("NewXMLConfigurationFromMap() failed, property %s not found", name)
+			} else if p.Value != value {
+				t.Errorf("NewXMLConfigurationFromMap() failed, expected property %s value %s, got %s", name, value, p.Value)
+			}
+		}
+	}
+}
+
+func TestAddProperty(t *testing.T) {
+	x := NewXMLConfiguration()
+
+	// Add a new property
+	p1 := Property{Name: "name1", Value: "value1"}
+	x.AddProperty(p1)
+
+	// Verify that the property was added
+	if len(x.Properties) != 1 {
+		t.Errorf("AddProperty() failed, expected 1 property, got %d", len(x.Properties))
+	} else if x.Properties[0] != p1 {
+		t.Errorf("AddProperty() failed, expected property %v, got %v", p1, x.Properties[0])
+	}
+
+	// Add another property with the same name
+	p2 := Property{Name: "name1", Value: "value2"}
+	x.AddProperty(p2)
+
+	// Verify that the existing property was updated
+	if len(x.Properties) != 1 {
+		t.Errorf("AddProperty() failed, expected 1 property, got %d", len(x.Properties))
+	} else if x.Properties[0] != p2 {
+		t.Errorf("AddProperty() failed, expected property %v, got %v", p2, x.Properties[0])
+	}
+
+	// Add a new property
+	p3 := Property{Name: "name2", Value: "value3"}
+	x.AddProperty(p3)
+
+	// Verify that the new property was added
+	if len(x.Properties) != 2 {
+		t.Errorf("AddProperty() failed, expected 2 properties, got %d", len(x.Properties))
+	} else if x.Properties[1] != p3 {
+		t.Errorf("AddProperty() failed, expected property %v, got %v", p3, x.Properties[1])
+	}
+}
+
+func TestAddPropertyWithString(t *testing.T) {
+	x := NewXMLConfiguration()
+
+	// Add a new property with string values
+	name := "name1"
+	value := "value1"
+	description := "description1"
+	comment := "This is a comment"
+	x.AddPropertyWithString(name, value, description, comment)
+
+	// Verify that the property was added
+	if len(x.Properties) != 1 {
+		t.Errorf("AddPropertyWithString() failed, expected 1 property, got %d", len(x.Properties))
+	} else {
+		p := x.Properties[0]
+		if p.Name != name || p.Value != value || p.Description != description || p.Comment != comment {
+			t.Errorf("AddPropertyWithString() failed, expected property %v, got %v", Property{Name: name, Value: value, Description: description, Comment: comment}, p)
+		}
+	}
+}
+
+func TestAddPropertiesWithMap(t *testing.T) {
+	x := NewXMLConfiguration()
+
+	// Define the properties map
+	properties := map[string]string{
+		"name1": "value1",
+		"name2": "value2",
+		"name3": "value3",
+	}
+
+	// Add properties using the map
+	x.AddPropertiesWithMap(properties)
+
+	// Verify that the properties were added
+	if len(x.Properties) != len(properties) {
+		t.Errorf("AddPropertiesWithMap() failed, expected %d properties, got %d", len(properties), len(x.Properties))
+	} else {
+		for name, value := range properties {
+			p, found := x.GetProperty(name)
+			if !found {
+				t.Errorf("AddPropertiesWithMap() failed, property %s not found", name)
+			} else if p.Value != value {
+				t.Errorf("AddPropertiesWithMap() failed, expected property %s value %s, got %s", name, value, p.Value)
+			}
+		}
+	}
+}
+
+func TestDeleteProperties(t *testing.T) {
+	x := NewXMLConfiguration()
+
+	// Add properties
+	p1 := Property{Name: "name1", Value: "value1"}
+	p2 := Property{Name: "name2", Value: "value2"}
+	p3 := Property{Name: "name3", Value: "value3"}
+	x.AddProperty(p1)
+	x.AddProperty(p2)
+	x.AddProperty(p3)
+
+	// Delete properties
+	x.DeleteProperties("name1", "name3")
+
+	// Verify that the properties were deleted
+	if len(x.Properties) != 1 {
+		t.Errorf("DeleteProperties() failed, expected 1 property, got %d", len(x.Properties))
+	} else if x.Properties[0] != p2 {
+		t.Errorf("DeleteProperties() failed, expected property %v, got %v", p2, x.Properties[0])
 	}
 }
