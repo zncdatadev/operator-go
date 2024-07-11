86fb1d63532824c19c6631077c70e01f02beb309
whg517 <huagang517@126.com>
2024-07-11 14:22:20 +0800

    feat(util): update generate random str and simple password functions (#72)

--- pkg/util/random.go
+++ pkg/util/random.go
@@ -1,44 +1,36 @@
 package util
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"errors"
-	mathRand "math/rand"
+	"math/rand"
 	"regexp"
 )
 
 const (
-	minioSecretIDLength = 40
+	letterBytes  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialBytes = "!@#$%^&*()_+-=[]{}\\|;':\",.<>/?`~"
+	numBytes     = "0123456789"
 )
 
-// GenerateSecretAccessKey - generate random base64 numeric value from a random seed.
-func GenerateSecretAccessKey() (string, error) {
-	rb := make([]byte, minioSecretIDLength)
-	if _, e := rand.Read(rb); e != nil {
-		return "", errors.New("could not generate Secret Key")
-	}
-
-	return string(Encode(rb)), nil
-}
-
-// Encode queues message
-func Encode(value []byte) []byte {
-	length := len(value)
-	encoded := make([]byte, base64.URLEncoding.EncodedLen(length))
-	base64.URLEncoding.Encode(encoded, value)
-	return encoded
-}
-
-// GenerateRandomStr - generate random string for given length
-func GenerateRandomStr(length int) string {
-	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// GenerateRandomStr generates a random string of the specified length.
+// The string can contain letters, special characters, and numbers based on the flags provided.
+func GenerateRandomStr(length int, useLetters bool, useSpecial bool, useNum bool) string {
 	bytes := make([]byte, length)
 	for i := range bytes {
-		bytes[i] = letterBytes[mathRand.Intn(len(letterBytes))]
+		if useLetters {
+			bytes[i] = letterBytes[rand.Intn(len(letterBytes))]
+		} else if useSpecial {
+			bytes[i] = specialBytes[rand.Intn(len(specialBytes))]
+		} else if useNum {
+			bytes[i] = numBytes[rand.Intn(len(numBytes))]
+		}
 	}
-
 	return string(bytes)
+}
+
+// GenerateSimplePassword generates a simple password of the specified length.
+// The password will contain only letters and numbers.
+func GenerateSimplePassword(length int) string {
+	return GenerateRandomStr(length, true, false, true)
 }
 
 // RemoveSpecialCharacter - remove special character
