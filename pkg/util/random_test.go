b6e4cf31527db26ede2aef7409fb0bc1191b7393
whg517 <huagang517@126.com>
2024-07-11 13:42:53 +0800

    feat(util): update generate random str and simple password functions

--- pkg/util/random_test.go
+++ pkg/util/random_test.go
@@ -1,8 +1,35 @@
 package util
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_generateRandomStr(t *testing.T) {
-	str := GenerateRandomStr(5)
-	t.Log(str)
+func TestGenerateRandomStr(t *testing.T) {
+	length := 10
+	useLetters := true
+	useSpecial := true
+	useNum := true
+
+	randomStr := GenerateRandomStr(length, useLetters, useSpecial, useNum)
+
+	if len(randomStr) != length {
+		t.Errorf("Generated random string length is incorrect. Expected: %d, Got: %d", length, len(randomStr))
+	}
+
+	// Add additional assertions here if needed
+
+	t.Logf("Generated random string: %s", randomStr)
+}
+
+func TestGenerateSimplePassword(t *testing.T) {
+	length := 8
+	password := GenerateSimplePassword(length)
+
+	if len(password) != length {
+		t.Errorf("Generated password length is incorrect. Expected: %d, Got: %d", length, len(password))
+	}
+
+	// Add additional assertions here if needed
+
+	t.Logf("Generated password: %s", password)
 }
