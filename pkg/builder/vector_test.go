40941e625934a8ea1c8ddb6b44e96456afe667ae
lwpk110 <lwpk110@163.com>
2024-07-26 16:31:27 +0800

    fix: fix missing indent formatting error in `vector.yaml`

--- pkg/builder/vector_test.go
+++ pkg/builder/vector_test.go
@@ -83,21 +83,21 @@ log_schema:
   host_key: "pod"
 sources:
   files_airlift:
-  type: "file"
-  include:
-    - "zncdata/log/*/*.airlift.json"
+    type: "file"
+    include:
+      - "zncdata/log/*/*.airlift.json"
 transforms:
   processed_files_airlift:
-  inputs:
-    - files_airlift
-  type: remap
-  source: |
-    parsed_event = parse_json!(string!(.message))
-    .message = join!(compact([parsed_event.message, parsed_event.stackTrace]), "\n")
-    .timestamp = parse_timestamp!(parsed_event.timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
-    .logger = parsed_event.logger
-    .level = parsed_event.level
-    .thread = parsed_event.thread
+    inputs:
+      - files_airlift
+    type: remap
+    source: |
+      parsed_event = parse_json!(string!(.message))
+      .message = join!(compact([parsed_event.message, parsed_event.stackTrace]), "\n")
+      .timestamp = parse_timestamp!(parsed_event.timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+      .logger = parsed_event.logger
+      .level = parsed_event.level
+      .thread = parsed_event.thread
   extended_logs_files:
     inputs:
       - processed_files_*
