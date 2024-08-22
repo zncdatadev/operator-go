6221e4769918a0f763ca792904f479927afad218
lwpk110 <lwpk110@163.com>
2024-08-22 11:45:55 +0800

    chore: standardize kubedoop-system directory paths with trailing slashes

--- pkg/util/bash.go
+++ pkg/util/bash.go
@@ -8,7 +8,7 @@ import (
 
 // Subdirectory of the log directory containing files to control the Vector instance
 const (
-	VectorLogDir = "_vector"
+	VectorLogDir = "_vector/"
 
 	// File to signal that Vector should be gracefully shut down
 	ShutdownFile = "shutdown"
@@ -51,21 +51,21 @@ wait_for_termination()
 // Use this command to remove the shutdown file (if it exists) created by [`create_vector_shutdown_file_command`].
 // You should execute this command before starting your application.
 func RemoveVectorShutdownFileCommand() string {
-	return fmt.Sprintf("rm -f %s/%s/%s", constants.KubedoopLogDir, VectorLogDir, ShutdownFile)
+	return fmt.Sprintf("rm -f %s%s%s", constants.KubedoopLogDir, VectorLogDir, ShutdownFile)
 }
 
 // Command to create a shutdown file for the vector container.
 // Please delete it before starting your application using `RemoveVectorShutdownFileCommand` .
 func CreateVectorShutdownFileCommand() string {
-	return fmt.Sprintf("mkdir -p %s/%s && touch %s/%s/%s", constants.KubedoopLogDir, VectorLogDir, constants.KubedoopLogDir, VectorLogDir, ShutdownFile)
+	return fmt.Sprintf("mkdir -p %s%s && touch %s%s%s", constants.KubedoopLogDir, VectorLogDir, constants.KubedoopLogDir, VectorLogDir, ShutdownFile)
 }
 
 // ExportPodAddress fetch the pod address from the default-address directory and export it as POD_ADDRESS
 // the listener was provided by listener operator
 func ExportPodAddress() string {
 	return fmt.Sprintf(`if [[ -d %s ]]; then
-  export POD_ADDRESS=$(cat %s/default-address/address)
-  for i in %s/default-address/ports/*; do
+  export POD_ADDRESS=$(cat %sdefault-address/address)
+  for i in %sdefault-address/ports/*; do
       export $(basename $i | tr a-z A-Z)_PORT="$(cat $i)"
   done
 fi`, constants.KubedoopListenerDir, constants.KubedoopListenerDir, constants.KubedoopListenerDir)
--- pkg/util/bash_test.go
+++ pkg/util/bash_test.go
@@ -29,7 +29,7 @@ var _ = Describe("RemoveVectorShutdownFileCommand", func() {
 var _ = Describe("CreateVectorShutdownFileCommand", func() {
 	It("should generate correct command string ", func() {
 		command := util.CreateVectorShutdownFileCommand()
-		Expect(command).Should(Equal("mkdir -p /kubedoop/log/_vector && touch /kubedoop/log/_vector/shutdown"))
+		Expect(command).Should(Equal("mkdir -p /kubedoop/log/_vector/ && touch /kubedoop/log/_vector/shutdown"))
 		Expect(strings.Contains(command, "\t")).To(BeFalse())
 	})
 })
@@ -38,7 +38,7 @@ var _ = Describe("ExportPodAddress", func() {
 	Context("correctly generates the command", func() {
 		It("supports valid logDir input", func() {
 			command := util.ExportPodAddress()
-			Expect(command).Should(Equal(`if [[ -d /kubedoop/listener ]]; then
+			Expect(command).Should(Equal(`if [[ -d /kubedoop/listener/ ]]; then
   export POD_ADDRESS=$(cat /kubedoop/listener/default-address/address)
   for i in /kubedoop/listener/default-address/ports/*; do
       export $(basename $i | tr a-z A-Z)_PORT="$(cat $i)"
