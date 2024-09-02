5ae61941217c262d3cce7f2dda967aefe0188dbd
steven <lwpk110@163.com>
2024-09-02 15:29:13 +0800

    fix(rbac): Correct rolebinding and clusterrolebinding generation, standardize resource naming (#140)
    * fix(reconciler): fix re-reconciler with 0 seconds not ready (#137)
    
    * fix(rbac): Correct rolebinding and clusterrolebinding generation, standardize resource naming
    
    - Fixed missing `subjects` and `roleRef` in RBAC, which caused incorrect generation of rolebinding and clusterrolebinding
    - Added helper functions to standardize RBAC resource naming, e.g., using `-serviceAccount`, `-rolebinding` suffixes
    
    ---------
    
    Co-authored-by: whg517 <huagang517@126.com>

--- pkg/apis/commons/v1alpha1/cluster_operation.go
+++ pkg/apis/commons/v1alpha1/cluster_operation.go
@@ -3,10 +3,10 @@ package v1alpha1
 // ClusterOperationSpec defines the desired state of ClusterOperation
 type ClusterOperationSpec struct {
 	// +kubebuilder:validation:Optional
-	// +kubebuilder:default:=false
+	// +kubebuilder:default=false
 	ReconciliationPaused bool `json:"reconciliationPaused,omitempty"`
 
 	// +kubebuilder:validation:Optional
-	// +kubebuilder:default:=false
+	// +kubebuilder:default=false
 	Stopped bool `json:"stopped,omitempty"`
 }
