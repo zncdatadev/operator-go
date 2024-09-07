102d5dfe889f1785da5f303b979ccc8320b3d461
whg517 <huagang517@126.com>
2024-09-07 11:50:04 +0800

    feat(constants): add enrichment and restarter labels from commons-operator

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -31,6 +31,48 @@ const (
 	KubedoopLogDirMount    = KubedoopRoot + "mount/log/"
 )
 
+// When a pod has the label `enrichment.zncdata.dev/enable=true`,
+// the enrichment controller will set the node address to the pod annotation when the pod is created.
+const (
+	LabelEnrichmentEnable      = "enrichment." + KubedoopDomain + "/enable"
+	LabelEnrichmentEnableValue = "true"
+	LabelEnrichmentNodeAddress = "enrichment." + KubedoopDomain + "/node-address"
+)
+
+// Restarter policy has workload restart and pod expiration.
+//
+// Workload restarter:
+//
+//	If a workload has the label `restarter.zncdata.dev/enable=true`,
+//	 and a configmap or secret is updated when mounted as a volume in the pod,
+//	 the restarter will update the annotations in the workload podTemplate.
+//	 The workload controller will update all the pods of the workload.
+//
+// Pod expiration:
+//
+//	When workload mount with secret-class of secret-operator, some secrets will be
+//	 created and mount for the pod by the secret-operator. Eg: kerberos, tls, etc.
+//	 Tls and kerberos secrets have expiration time, when the secrets is created,
+//	 secret-operator will set the expiration time in the pod annotation.
+//	 The restarter will check the expiration time in the pod annotation, if the expiration time is expired,
+//	 the restarter will restart the pod.
+const (
+	LabelRestarterEnable      = "restarter." + KubedoopDomain + "/enable"
+	LabelRestarterEnableValue = "true"
+
+	// eg:
+	// 	- secret.restarter.zncdata.dev/foo-secret: <secret-uuid>/<secret-resourceversion>
+	// 	- configmap.restarter.zncdata.dev/foo-configmap: <configmap-uuid>/<configmap-resourceversion>
+
+	AnnotationSecretRestarterPrefix    = "secret.restarter." + KubedoopDomain + "/"
+	AnnotationConfigmapRestarterPrefix = "configmap.restarter." + KubedoopDomain + "/"
+
+	// eg:
+	// 	- restarter.zncdata.dev/expires-at.<RFC3339>: <volume-id>
+	// RFC3339: 2006-01-02T15:04:05Z07:00
+	PrefixLabelRestarterExpiresAt = "restarter." + KubedoopDomain + "/expires-at."
+)
+
 func MatchingLabelsNames() []string {
 	return []string{
 		LabelKubernetesName,
