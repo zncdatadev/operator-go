68683fa74e0f986bf7baf5985dc1bc791f1b4099
steven <lwpk110@163.com>
2024-12-04 19:32:13 +0800

    refactor(api): remove kubebuilder validation tags to fix CR installation failure (#273)
    - Removed `// +kubebuilder:pruning:PreserveUnknownFields`
    - Removed `// +kubebuilder:validation:EmbeddedResource`

--- pkg/apis/commons/v1alpha1/overrides_types.go
+++ pkg/apis/commons/v1alpha1/overrides_types.go
@@ -28,8 +28,6 @@ type OverridesSpec struct {
 	// +kubebuilder:validation:Optional
 	ConfigOverrides map[string]map[string]string `json:"configOverrides,omitempty"`
 	// +kubebuilder:validation:Optional
-	// +kubebuilder:pruning:PreserveUnknownFields
-	// +kubebuilder:validation:EmbeddedResource
 	// +kubebuilder:validation:Type=object
 	PodOverrides *k8sruntime.RawExtension `json:"podOverrides,omitempty"`
 }
